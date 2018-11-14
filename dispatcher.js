2b1355226e48c16cebd20024632ddf712068df26
Your Name <you@example.com>
2018-11-14 20:59:57 +0800

    1

---
+++ dispatcher.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'task_queue';
+    var msg = JSON.stringify({q})
+
+    ch.assertQueue(q, {durable: true});
+    ch.sendToQueue(q, new Buffer.from(msg), {persistent: true});
+    console.log(" [x] Sent '%s'", msg);
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
